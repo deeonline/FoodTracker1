7ad7dd47f326ffd3b10eb2726ecbd29888644f12
deeonline <deekay@Dees-MacBook-Air.local>
2016-08-30 00:01:25 +0200

    1. Initial commit

---
+++ FoodTracker/RatingStars.swift
@@ -0,0 +1,21 @@
+//
+//  RatingStars.swift
+//  FoodTracker
+//
+//  Created by Dee Kay on 8/29/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class RatingStars: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
